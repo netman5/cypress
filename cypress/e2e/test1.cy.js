d0b0bcdebed52014ebcd6b602723347888434636
Ola Ishola <olaishola@hotmail.co.uk>
2023-01-12 14:00:21 +0100

    First test with cypress

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+it('Google Search', () => {
+  cy.visit('https://google.com');
+  cy.get('.gLFyf').type('How much is iphone 14 pro in nigeria {enter}');
+  cy.wait(5000);
+  cy.contains('Specs').click();
+  // cy.contains('Google Search').click();
+})
